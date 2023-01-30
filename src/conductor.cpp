97c21245bde6580f780809f415038aaff2043d1a
Richard Tai <tzu.huan.tai@gmail.com>
2023-01-30 17:54:07 +0800

    style: rearrange header

--- src/conductor.cpp
+++ src/conductor.cpp
@@ -4,31 +4,25 @@
 
 #include <api/audio_codecs/builtin_audio_decoder_factory.h>
 #include <api/audio_codecs/builtin_audio_encoder_factory.h>
+#include <api/call/call_factory_interface.h>
 #include <api/create_peerconnection_factory.h>
+#include <api/peer_connection_interface.h>
+#include <api/rtc_event_log/rtc_event_log_factory.h>
+#include <api/transport/field_trial_based_config.h>
+#include <api/task_queue/default_task_queue_factory.h>
 #include <api/video_codecs/builtin_video_decoder_factory.h>
 #include <api/video_codecs/builtin_video_encoder_factory.h>
 #include <pc/video_track_source_proxy.h>
+#include <media/engine/webrtc_media_engine.h>
 #include <modules/audio_device/include/audio_device.h>
 #include <modules/audio_device/include/audio_device_factory.h>
 #include <modules/audio_device/linux/audio_device_alsa_linux.h>
 #include <modules/audio_device/linux/audio_device_pulse_linux.h>
 #include <modules/audio_device/linux/latebindingsymboltable_linux.h>
+#include <modules/audio_processing/include/audio_processing.h>
+#include <rtc_base/logging.h>
 #include <rtc_base/ssl_adapter.h>
 #include <rtc_base/thread.h>
-
-#include <api/rtc_event_log/rtc_event_log_factory.h>
-#include <api/task_queue/default_task_queue_factory.h>
-#include <media/engine/webrtc_media_engine.h>
-#include <modules/audio_processing/include/audio_processing.h>
-#include <modules/video_capture/video_capture.h>
-#include <modules/video_capture/video_capture_factory.h>
-#include <p2p/client/basic_port_allocator.h>
-#include <pc/peer_connection_factory_proxy.h>
-#include <rtc_base/logging.h>
-
-#include <api/call/call_factory_interface.h>
-#include <api/peer_connection_interface.h>
-#include <api/transport/field_trial_based_config.h>
 
 Conductor::Conductor(Args args) : args(args)
 {
