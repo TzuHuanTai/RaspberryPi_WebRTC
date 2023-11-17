21cee28d43350ed1f6b47000d3a6b331e9573842
TzuHuanTai <tzu.huan.tai@gmail.com>
2023-11-17 20:45:38 +0800

    chore: remove display connecting time

--- src/main.cpp
+++ src/main.cpp
@@ -33,21 +33,12 @@ int main(int argc, char *argv[]) {
         #endif
         })();
 
-        std::cout << "=> main: wait for ready streaming!" << std::endl;
+        std::cout << "=> main: wait for signaling!" << std::endl;
         std::unique_lock<std::mutex> lock(conductor->mtx);
         conductor->cond_var.wait(lock, [&]{return conductor->is_ready_for_streaming;});
 
-        auto start = std::chrono::system_clock::now();
-        std::time_t end_time;
-        while (conductor->is_ready_for_streaming) {
-            auto end = std::chrono::system_clock::now();
-            std::chrono::duration<double> elapsed_seconds = end-start;
-            end_time = std::chrono::system_clock::to_time_t(end);
-            std::cout << '\r' << "running time: " << elapsed_seconds.count() << "s " << "\e[K" << std::flush;
-            usleep(100000);
-        }
-        
-        std::cout << std::ctime(&end_time);
+        sleep(1);
+        conductor->cond_var.wait(lock, [&]{return !conductor->is_ready_for_streaming;});
     }
     
     return 0;
