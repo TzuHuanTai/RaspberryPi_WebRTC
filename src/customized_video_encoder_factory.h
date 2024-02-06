2112a24de67af9f55799de4ed4995909ce94cff1
TzuHuanTai <tzu.huan.tai@gmail.com>
2024-02-06 04:33:28 -0800

    refactor: move openh264 to customized factory

--- src/customized_video_encoder_factory.h
+++ src/customized_video_encoder_factory.h
@@ -1,27 +1,15 @@
 #ifndef CUSTOMIZED_VIDEO_ENCODER_FACTORY_H_
 #define CUSTOMIZED_VIDEO_ENCODER_FACTORY_H_
 
-#include "data_channel_subject.h"
 #include "args.h"
 
-#include <api/video_codecs/sdp_video_format.h>
-#include <api/video_codecs/video_encoder.h>
 #include <api/video_codecs/video_encoder_factory.h>
-#include <media/engine/simulcast_encoder_adapter.h>
-#include <media/engine/internal_encoder_factory.h>
 
-#include <memory>
-#include <vector>
-
-std::unique_ptr<webrtc::VideoEncoderFactory> CreateCustomizedVideoEncoderFactory(
-    Args args, std::shared_ptr<DataChannelSubject> data_channel_subject);
+std::unique_ptr<webrtc::VideoEncoderFactory> CreateCustomizedVideoEncoderFactory(Args args);
 
 class CustomizedVideoEncoderFactory : public webrtc::VideoEncoderFactory {
 public:
-    CustomizedVideoEncoderFactory(Args args, 
-        std::shared_ptr<DataChannelSubject> data_channel_subject)
-        : args_(args),
-          data_channel_subject_(data_channel_subject) {};
+    CustomizedVideoEncoderFactory(Args args) : args_(args) {};
     ~CustomizedVideoEncoderFactory() {};
 
     std::vector<webrtc::SdpVideoFormat> GetSupportedFormats() const override;
@@ -31,7 +19,6 @@ public:
 
 private:
     Args args_;
-    std::shared_ptr<DataChannelSubject> data_channel_subject_;
 };
 
 #endif // CUSTOMIZED_VIDEO_ENCODER_FACTORY_H_
