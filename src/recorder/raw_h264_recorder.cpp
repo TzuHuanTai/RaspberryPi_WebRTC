4525b7accd032f6db10d8acee2196c8bc9f2635d
TzuHuanTai <tzu.huan.tai@gmail.com>
2024-05-20 06:39:31 -0700

    fix: check nal type

--- src/recorder/raw_h264_recorder.cpp
+++ src/recorder/raw_h264_recorder.cpp
@@ -1,5 +1,10 @@
 #include "recorder/raw_h264_recorder.h"
 
+#include <cstdint>
+#define NAL_UNIT_TYPE_IDR 5
+#define NAL_UNIT_TYPE_SPS 7
+#define NAL_UNIT_TYPE_PPS 8
+
 const int SECOND_PER_FILE = 300;
 
 std::unique_ptr<RawH264Recorder> RawH264Recorder::Create(Args config) {
@@ -9,10 +14,62 @@ std::unique_ptr<RawH264Recorder> RawH264Recorder::Create(Args config) {
 }
 
 RawH264Recorder::RawH264Recorder(Args config, std::string encoder_name)
-    : VideoRecorder(config, encoder_name) {};
+    : VideoRecorder(config, encoder_name),
+      has_sps_(false),
+      has_pps_(false),
+      has_first_keyframe_(false) {};
 
 RawH264Recorder::~RawH264Recorder() {}
 
+void RawH264Recorder::ResetCodecs() {
+    has_sps_ = false;
+    has_pps_ = false;
+    has_first_keyframe_ = false;
+}
+
 void RawH264Recorder::Encode(Buffer buffer) {
-    OnEncoded(buffer);
+    if (buffer.flags & V4L2_BUF_FLAG_KEYFRAME && !has_first_keyframe_ ) {
+        CheckNALUnits(buffer);
+        if (has_sps_ && has_pps_) {
+            has_first_keyframe_ = true;
+        }
+    }
+
+    if (has_first_keyframe_) {
+        OnEncoded(buffer);
+    }
+}
+
+bool RawH264Recorder::CheckNALUnits(const Buffer& buffer) {
+    if (buffer.start == nullptr || buffer.length < 4) {
+        return false;
+    }
+
+    uint8_t* data = static_cast<uint8_t*>(buffer.start);
+    for (unsigned int i = 0; i < buffer.length - 4; ++i) {
+        if (data[i] == 0x00 && data[i + 1] == 0x00 &&
+            ((data[i + 2] == 0x01) || (data[i + 2] == 0x00 && data[i + 3] == 0x01))) {
+            // Found start code
+            size_t startCodeSize = (data[i + 2] == 0x01) ? 3 : 4;
+            uint8_t nalUnitType = data[i + startCodeSize] & 0x1F;
+            switch (nalUnitType) {
+                case NAL_UNIT_TYPE_IDR:
+                    printf("Found IDR frame\n");
+                    break;
+                case NAL_UNIT_TYPE_SPS:
+                    printf("Found SPS NAL unit\n");
+                    has_sps_ = true;
+                    break;
+                case NAL_UNIT_TYPE_PPS:
+                    printf("Found PPS NAL unit\n");
+                    has_pps_ = true;
+                    break;
+                default:
+                    break;
+            }
+            i += startCodeSize;
+        }
+    }
+
+    return false;
 }
