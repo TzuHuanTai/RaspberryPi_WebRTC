b06fa7aca28484457ab831903ba8acde02f73afb
TzuHuanTai <tzu.huan.tai@gmail.com>
2024-05-20 06:39:31 -0700

    fix: check nal type

--- src/recorder/raw_h264_recorder.h
+++ src/recorder/raw_h264_recorder.h
@@ -8,9 +8,16 @@ public:
     static std::unique_ptr<RawH264Recorder> Create(Args config);
     RawH264Recorder(Args config, std::string encoder_name);
     ~RawH264Recorder();
+    void ResetCodecs() override;
+    bool CheckNALUnits(const Buffer& buffer);
 
 protected:
     void Encode(Buffer buffer) override;
+
+private:
+    bool has_sps_;
+    bool has_pps_;
+    bool has_first_keyframe_;
 };
 
 #endif
