869c45776642359ff4b043cb8522cee0300e67e4
TzuHuanTai <tzu.huan.tai@gmail.com>
2024-07-02 06:50:55 -0700

    test: save and convert i420 to jpeg

--- src/common/utils.cpp
+++ src/common/utils.cpp
@@ -1,5 +1,8 @@
 #include "utils.h"
 
+#include <third_party/libyuv/include/libyuv.h>
+#include <jpeglib.h>
+
 bool Utils::CreateFolder(const std::string& folder_path) {
     if (folder_path.empty()) {
         return false;
@@ -75,4 +78,75 @@ bool Utils::CheckDriveSpace(const std::string &file_path, unsigned long min_free
     unsigned long free_space_mb = free_space / (1024 * 1024);
 
     return free_space_mb >= min_free_space_mb;
+}
+
+std::string Utils::PrefixZero(int src, int digits) {
+    std::string str = std::to_string(src);
+    std::string n_zero(digits - str.length(), '0');
+    return n_zero + str;
+}
+
+std::string Utils::GenerateFilename() {
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+
+    std::string year = Utils::PrefixZero(1900 + ltm->tm_year, 4);
+    std::string month = Utils::PrefixZero(1 + ltm->tm_mon, 2);
+    std::string day = Utils::PrefixZero(ltm->tm_mday, 2);
+    std::string hour = Utils::PrefixZero(ltm->tm_hour, 2);
+    std::string min = Utils::PrefixZero(ltm->tm_min, 2);
+    std::string sec = Utils::PrefixZero(ltm->tm_sec, 2);
+
+    std::stringstream s1;
+    std::string filename;
+    s1 << year << month << day << "_" << hour << min << sec;
+    s1 >> filename;
+
+    return filename;
+}
+
+Buffer Utils::ConvertYuvToJpeg(const uint8_t* yuv_data, int width, int height) {
+    struct jpeg_compress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+
+    Buffer jpegBuffer = { NULL, 0 };
+
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_compress(&cinfo);
+    uint8_t* data = NULL;
+    unsigned long size = 0;
+    jpeg_mem_dest(&cinfo, &data, &size);
+
+    cinfo.image_width = width;
+    cinfo.image_height = height;
+    cinfo.input_components = 3;
+    cinfo.in_color_space = JCS_EXT_BGR;
+
+    jpeg_set_defaults(&cinfo);
+    jpeg_set_quality(&cinfo, 100, TRUE);
+
+    JSAMPROW row_pointer[1];
+    int row_stride = width * 3;
+    uint8_t* rgb_data = (uint8_t*)malloc(width * height * 3);
+    libyuv::I420ToRGB24(yuv_data, width,
+                        yuv_data + width * height, width / 2,
+                        yuv_data + width * height + (width * height / 4), width / 2,
+                        rgb_data, width * 3,
+                        width, height);
+
+    jpeg_start_compress(&cinfo, TRUE);
+
+    while (cinfo.next_scanline < cinfo.image_height) {
+        row_pointer[0] = &rgb_data[cinfo.next_scanline * row_stride];
+        jpeg_write_scanlines(&cinfo, row_pointer, 1);
+    }
+
+    jpeg_finish_compress(&cinfo);
+    jpeg_destroy_compress(&cinfo);
+    free(rgb_data);
+
+    jpegBuffer.start = data;
+    jpegBuffer.length = size;
+
+    return jpegBuffer;
 }
--- src/common/utils.h
+++ src/common/utils.h
@@ -10,13 +10,21 @@
 #include <filesystem>
 #include <sys/statvfs.h>
 
+struct Buffer {
+    void *start = nullptr;
+    unsigned int length;
+};
+
 class Utils {
 public:
-    static bool CreateFolder(const std::string& folder_path);
+    static std::string GenerateFilename();
+    static std::string PrefixZero(int src, int digits);
     static std::string ToBase64(const std::string &binary_file);
     static std::string ReadFileInBinary(const std::string &file_path);
     static std::string FindLatestJpg(const std::string &directory);
+    static bool CreateFolder(const std::string& folder_path);
     static bool CheckDriveSpace(const std::string &file_path, unsigned long min_free_space_mb);
+    static Buffer ConvertYuvToJpeg(const uint8_t* yuv_data, int width, int height);
 };
 
 #endif // UTILS_
