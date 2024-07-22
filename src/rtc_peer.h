ea50232d308032a96782bc8f1e073ae03c2dc33b
TzuHuanTai <tzu.huan.tai@gmail.com>
2024-07-21 21:39:16 -0700

    feat: peer connection timeout in 10 seconds

--- src/rtc_peer.h
+++ src/rtc_peer.h
@@ -8,6 +8,7 @@
 #include <api/peer_connection_interface.h>
 #include <api/video/video_sink_interface.h>
 
+#include <atomic>
 #include <iostream>
 
 class SetSessionDescription : public webrtc::SetSessionDescriptionObserver {
@@ -98,7 +99,7 @@ private:
     void OnRemoteIce(std::string sdp_mid, int sdp_mline_index, std::string candidate) override;
 
     int id_;
-    bool is_connected_;
+    std::atomic<bool> is_connected_;
     bool is_ready_to_connect_;
     std::shared_ptr<SignalingService> signaling_client_;
     std::shared_ptr<DataChannelSubject> data_channel_subject_;
